44d63838124ba0acae618010d47857f2f079208c
Italianorafa <italianorafap@gmail.com>
2025-04-19 17:28:53 +0100

    add test 1 files

---
+++ EDA/Testes ano passado/Teste 1/ex1/ex1.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+int insert_several(vector<int>& v, int n){
+    // questão 1 a)
+    return 0;
+}
+
+
+int insert_pos(vector<int>& v, int pos, int value){
+    //questão 1 b)
+    return 0;
+}
+
+// Utility function to print the vector
+void print_vec(vector<int>& v){
+    cout << "Vector elements: " << endl;
+    for (auto i = v.begin(); i != v.end(); ++i)
+        cout << *i << " ";
+    cout << endl; 
+}
+
+
+
+int main(){
+    
+
+    vector<int> numbers;
+    cout << "Return Value: " << insert_several(numbers,10) << endl;
+    print_vec(numbers);
+
+    int pos = 3;
+    int value = 4;
+    cout << "Return Value: " << insert_pos(numbers, pos, value)<< endl;
+    print_vec(numbers);
+
+
+}
